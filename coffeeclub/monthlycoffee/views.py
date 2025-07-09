34e42afbc14f2155a8c77386b2619db5da28924f
Berta devant <bertadevant@Bertas-MacBook-Air.local>
2025-07-09 19:18:35 +0200

    create random coffee banner

--- coffeeclub/monthlycoffee/views.py
+++ coffeeclub/monthlycoffee/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
+from django.templatetags.static import static
 from . import get_roasters_data
 from . import set_roaster_data
+import random
 
 def index(request):
     roaster = get_roasters_data.get_last_month_roaster()
+    banner_num = random.randint(1, 30)
+    banner_url = static(f"monthlycoffee/images/banner_images/{banner_num}.jpg")
     template = 'monthlycoffee/index.html'
-    context = {"last_month_roaster": roaster}
+    context = {
+        "last_month_roaster": roaster,
+        "banner_url": banner_url
+    }
     return render(request, template, context)
 
 def results(request):
